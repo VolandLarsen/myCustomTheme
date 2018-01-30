fe5b723982e548b8ee4b0a44a4eb72419a7027cc
VolandLarsen <nitro1493@gmail.com>
2018-01-30 15:32:18 +0200

    Add scripts and styles

--- index.php
+++ index.php
@@ -14,43 +14,144 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<section class="error_top_bg secondary-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="top_banner">
+                    <div class="category_m">
+                        <h2> App News</h2>
 
-		<?php
-		if ( have_posts() ) :
+                        <div class="breadcrumb_m">
+                            <ul class="breadcrumbs_m" id="breadcrumbs_m">
+                                <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> Home </a></li>
+                                <li> / </li>
+                                <li><a href="#">  Blog</a></li>
+                            </ul>
+                        </div>
+                    </div>
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
-			<?php
-			endif;
 
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+<section class="post_blog_bg primary-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
 
-			endwhile;
+                <div class="col-md-8">
+                    <article class="blog_post">
+                        <h4> <a href="#"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. </a> </h4>
 
-			the_posts_navigation();
+                        <div class="blog_category">
+                            <ul>
+                                <li> <a href="#">Company news, </a> </li>
+                                <li> <a href="#">Fresh releases </a> </li>
+                            </ul>
+                        </div>
 
-		else :
+                        <div class="blog_text">
+                            <ul>
+                                <li> | </li>
+                                <li> <a href="#"> Post By : Admin   </a> </li>
+                                <li> | </li>
+                                <li>  On : 20 may 2016 </li>
+                            </ul>
+                        </div>
 
-			get_template_part( 'template-parts/content', 'none' );
+                        <div class="blog_post_img">
+                            <a href="#"> <img src="images/blog_post_img1.png" alt="image"> </a>
+                        </div>
 
-		endif; ?>
+                        <p> Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. </p>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+                        <a href="#"> Continue reading <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
 
-<?php
-get_sidebar();
-get_footer();
+
+                    </article>
+
+                    <article class="blog_post">
+                        <h4> <a href="#"> There are many variations of passages of Lorem Ipsum available. </a> </h4>
+
+                        <div class="blog_category">
+                            <ul>
+                                <li> <a href="#">Company news, </a> </li>
+                                <li> <a href="#">Fresh releases </a> </li>
+                            </ul>
+                        </div>
+
+                        <div class="blog_text">
+                            <ul>
+                                <li> | </li>
+                                <li> <a href="#"> Post By : Admin   </a> </li>
+                                <li> | </li>
+                                <li>  On : 20 may 2016 </li>
+                            </ul>
+                        </div>
+
+                        <div class="blog_post_img">
+                            <a href="#"> <img src="images/blog_post_img2.png" alt="image"> </a>
+                        </div>
+
+                        <p> There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary. </p>
+
+                        <a href="#"> Continue reading <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                    </article>
+
+                    <article class="blog_post">
+                        <h4> <a href="#"> All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks </a></h4>
+
+                        <div class="blog_category">
+                            <ul>
+                                <li> <a href="#">Company news, </a> </li>
+                                <li> <a href="#">Fresh releases </a> </li>
+                            </ul>
+                        </div>
+
+                        <div class="blog_text">
+                            <ul>
+                                <li> | </li>
+                                <li> <a href="#"> Post By : Admin   </a> </li>
+                                <li> | </li>
+                                <li>  On : 20 may 2016 </li>
+                            </ul>
+                        </div>
+
+                        <div class="blog_post_img">
+                            <a href="#">	<img src="images/blog_post_img3.png" alt="image"> </a>
+                        </div>
+
+                        <p> All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc. </p>
+
+                        <a href="#"> Continue reading <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                    </article>
+
+                    <div class="next_page">
+                        <ul class="page-numbers">
+                            <li><span class="page-numbers current">1</span></li>
+                            <li><a href="#" class="page-numbers">2</a></li>
+                            <li><a href="#" class="page-numbers">3</a></li>
+                            <li><a href="#" class="page-numbers">4</a></li>
+                            <li><a href="#" class="next page-numbers">Next</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+
+                <!--sidebar-->
+
+               <?php get_sidebar(); ?>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<?php get_footer(); ?>
